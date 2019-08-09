58fe356c398358d8cde39890a7d87b1d6acea0d2
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-08 17:42:01 -0700

    formatted document

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,14 +4,8 @@ var inquirer = require('inquirer');
 // create connection to mysql
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 8889,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "root",
     database: "bamazon"
 });
