e6005a07ba880b284fb6bef378cd587eb8796008
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-09 23:54:18 -0700

    cleaned up notes; prep for supervisor view

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,11 @@
+// The app should then prompt users with two messages.
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+// This means updating the SQL database to reflect the remaining quantity.
+
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var Table = require('cli-table');
@@ -10,7 +18,6 @@ var connection = mysql.createConnection({
     password: "root",
     database: "bamazon"
 });
-
 
 // connect to mysql
 function connectSQL() {
@@ -84,7 +91,7 @@ function purchase(productID, quantity) {
         if (err) throw err;
         var updatedQuantity = item[0].stock_quantity - quantity;
         var totalSale = item[0].price * item[0].stock_quantity;
-        console.log(`Purchase of ${updatedQuantity}x ${item[0].product_name} complete!`);
+        console.log(`Purchase of ${quantity}x ${item[0].product_name} complete!`);
         console.log(`Total: $${totalSale}`);
         console.log(`Previous Quantity: ${item[0].stock_quantity}`);
         console.log(`Updated Quantity: ${updatedQuantity}`);
@@ -152,7 +159,7 @@ function quit() {
     process.exit();
 };
 
-
+// CLI TABLE
 
 // // instantiate
 // var table = new Table({
@@ -170,36 +177,7 @@ function quit() {
 
 welcome();
 
-// The app should then prompt users with two messages.
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-// This means updating the SQL database to reflect the remaining quantity.
-
-
-
-// -------------------
-// Challenge #2: Manager View (Next Level)
-// Create a new Node application called bamazonManager.js. Running this application will:
-
-// List a set of menu options:
-// View Products for Sale
-// View Low Inventory
-// Add to Inventory
-// Add New Product
-
-// If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
-// If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
-// If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
-// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
-
+// ADD NEW TABLE TO PRODUCTS
 // -------------------
 // Challenge #3: Supervisor View (Final Level)
 
@@ -210,34 +188,4 @@ welcome();
 
 // Modify the products table so that there's a product_sales column, and modify your bamazonCustomer.js app so that when a customer purchases anything from the store, the price of the product multiplied by the quantity purchased is added to the product's product_sales column.
 // Make sure your app still updates the inventory listed in the products column.
-
-// Create another Node app called bamazonSupervisor.js. Running this application will list a set of menu options:
-// View Product Sales by Department
-// Create New Department
-
-// When a supervisor selects View Product Sales by Department, the app should display a summarized table in their terminal/bash window. Use the table below as a guide.
-// department_id
-// department_name
-// over_head_costs
-// product_sales
-// total_profit
-
-// 01
-// Electronics
-// 10000
-// 20000
-// 10000
-
-// 02
-// Clothing
-// 60000
-// 100000
-// 40000
-
-// The total_profit column should be calculated on the fly using the difference between over_head_costs and product_sales. total_profit should not be stored in any database. You should use a custom alias.
-// If you can't get the table to display properly after a few hours, then feel free to go back and just add total_profit to the departments table.
-// Hint: You may need to look into aliases in MySQL.
-// Hint: You may need to look into GROUP BYs.
-// Hint: You may need to look into JOINS.
-// HINT: There may be an NPM package that can log the table to the console. What's is it? Good question :)
 
