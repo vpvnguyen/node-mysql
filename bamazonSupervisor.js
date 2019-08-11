1cbcd97365286210282f998d9712e1435485fff4
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-11 13:05:58 -0700

    queried view product sales by joining 2 tables to calculate total profit

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,3 +1,6 @@
+// When a supervisor selects View Product Sales by Department, the app should display a summarized table in their terminal/bash window.
+// The total_profit column should be calculated on the fly using the difference between over_head_costs and product_sales. total_profit is NOT stored in any database.
+
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var Table = require('cli-table');
@@ -41,18 +44,33 @@ function superView() {
     });
 };
 
-// department_id 01 02
-// department_name Electronics Clothing - department
-// over_head_costs 10000 60000 - products
-// product_sales 20000 100000 - products
-// total_profit 10000 40000 - product_sales(prod) - over_head_costs(prod)
-
-// get product sales by 
+// get product sales by joining departments and products table; calculate total profit from product_sales - over_head_costs
 function productSales() {
-    console.log('product sales');
-    console.log('\n');
-    superView();
 
+    // join products and departments tables as non duplicates; calculate total profit from product_sales - over_head_costs; list by department_id
+    var query =
+        `SELECT department_id, departments.department_name, over_head_costs, SUM(product_sales) AS product_sales, SUM(product_sales) - over_head_costs AS total_profit 
+        FROM departments 
+        INNER JOIN products ON departments.department_name = products.department_name 
+        GROUP BY department_id`
+
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        var table = new Table({
+            head: ['department_id', 'department_name', 'over_head_costs', 'product_sales', 'total_profit'],
+            colWidths: [15, 20, 20, 18, 18]
+        });
+
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].product_sales, res[i].total_profit]);
+        }
+
+        console.log('\n');
+        console.log(table.toString());
+        console.log('Here are all of the coss and sales of each deparment!');
+        superView();
+    });
 };
 
 // creates a new department in departments db
@@ -71,21 +89,3 @@ function quit() {
 };
 
 startApp();
-
-// Modify the products table so that there's a product_sales column, and modify your bamazonCustomer.js app so that when a customer purchases anything from the store, the price of the product multiplied by the quantity purchased is added to the product's product_sales column.
-// Make sure your app still updates the inventory listed in the products column.
-
-// When a supervisor selects View Product Sales by Department, the app should display a summarized table in their terminal/bash window. Use the table below as a guide.
-// department_id 01 02
-// department_name Electronics Clothing
-// over_head_costs 10000 60000
-// product_sales 20000 100000
-// total_profit 10000 40000
-// total profit = product_sales - over_head_costs ON THE FLY
-
-// The total_profit column should be calculated on the fly using the difference between over_head_costs and product_sales. total_profit should not be stored in any database. You should use a custom alias.
-// If you can't get the table to display properly after a few hours, then feel free to go back and just add total_profit to the departments table.
-// Hint: You may need to look into aliases in MySQL.
-// Hint: You may need to look into GROUP BYs.
-// Hint: You may need to look into JOINS.
-// HINT: There may be an NPM package that can log the table to the console. What's is it? Good question :)
