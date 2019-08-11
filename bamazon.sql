788c9a754380e939a2306a102afa642dfe8d4c61
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-11 15:59:40 -0700

    added full query; create db, create products and departments table

--- bamazon.sql
+++ bamazon.sql
@@ -1,3 +1,4 @@
+-- create db with products table --
 DROP DATABASE IF EXISTS bamazon;
 
 CREATE DATABASE bamazon;
@@ -11,28 +12,56 @@ CREATE TABLE products
     product_name TINYTEXT NOT NULL,
     department_name TINYTEXT NOT NULL,
     price INT NOT NULL,
-    stock_quantity INT NOT NULL
+    stock_quantity INT NOT NULL,
+    product_sales INT NOT NULL
 );
 
+    -- reuse insert query to add new products
     INSERT INTO products
-        (product_name, department_name, price, stock_quantity)
+        (
+        product_name,
+        department_name,
+        price,
+        stock_quantity,
+        product_sales
+        )
     VALUE
-    ('Coleman 2-Person Tent',
-    'Outdoor',
-    90,
-    10
-    ),
-    ('Headlamp', 'Electronics', 20, 2000),
-    ('Go Pro', 'Electronics', 500, 5),
-    ('Yeti Cooler', 'Outdoor', 50, 15),
-    ('Shimano Fishing Pole', 'Sporting Goods', 50, 100),
-    ('Hobie Kayak', 'Sporting Goods', 2000, 2),
-    ('Propane', 'Outdoor', 30, 10),
-    ('Water', 'Food', 12, 600),
-    ('Sleeping Bag', 'Outdoor', 40, 50),
-    ('Toyota Tacoma', 'Automotive', 35000, 3);
+    (
+    'Toyota Tacoma',
+    'Automotive',
+    35000,
+    3,
+    0
+    );
 
-    SELECT *
-    FROM products;
+    -- create departments table
+    CREATE TABLE departments
+    (
+        department_id INT NOT NULL
+        AUTO_INCREMENT PRIMARY KEY,
+    department_name TINYTEXT NOT NULL,
+    over_head_costs INT NOT NULL
+);
+
+
+        INSERT INTO departments
+            (
+            department_name,
+            over_head_costs
+            )
+        VALUE
+        (
+        'Food',
+        1100
+        );
+
+        SELECT *
+        FROM products;
+
+        SELECT *
+        FROM departments;
+
+        SELECT *
+        FROM products, departments;
 
 -- END --
